098c2d42bf7eeebd6a50d1c125fa7c9a1bb0edcc
Nico Devs <nico1485@gmail.com>
2024-07-03 14:20:28 -0300

    Add styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Customers</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
         <livewire:customer-search />
