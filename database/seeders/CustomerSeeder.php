23760c1bf60192e3dbd3b61718f356b4b0de51e7
Nico Devs <nico1485@gmail.com>
2024-11-13 09:42:51 -0300

    Add Typesense

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -13,11 +13,12 @@ class CustomerSeeder extends Seeder
      */
     public function run(): void
     {
-        $total = 2_000_000;
-        $chunkSize = 100_000;
-
-        for ($i = 0; $i < $total; $i += $chunkSize) {
-            Customer::factory()->count($chunkSize)->create();
-        }
+        Customer::withoutSyncingToSearch(function () {
+            $total = 250_000;
+            $chunkSize = 10_000;
+            for ($i = 0; $i < $total; $i += $chunkSize) {
+                Customer::factory()->count($chunkSize)->create();
+            }
+        });
     }
 }
